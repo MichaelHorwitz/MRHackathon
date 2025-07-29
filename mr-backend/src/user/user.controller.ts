d1df0510ffa1c86e3c4e49b8c19a64fd962c72e5
Caelan Hill <caelan.hill@gmail.com>
2025-07-29 23:30:54 +0200

    secured user ednpoints

--- mr-backend/src/user/user.controller.ts
+++ mr-backend/src/user/user.controller.ts
@@ -10,17 +10,22 @@ import {
   ParseIntPipe,
   NotFoundException,
   InternalServerErrorException,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   /** GET /users/:id */
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<User> {
     const user = await this.userService.findOne(id);
@@ -31,12 +36,16 @@ export class UserController {
   }
 
   /** POST /users */
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @Post()
   async create(@Body() dto: CreateUserDto): Promise<User> {
     return this.userService.create(dto);
   }
 
   /** PUT /users/:id */
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @Put(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -50,6 +59,8 @@ export class UserController {
   }
 
   /** DELETE /users/:id */
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.userService.remove(id);
