77aa95f8a70871d78b5b1133624ba7e6d2d69a6d
Vincent <vafdaf11@gmail.com>
2025-07-29 14:47:55 +0200

    feat: Output form data on login

--- frontend/src/app/login/action.ts
+++ frontend/src/app/login/action.ts
@@ -1,4 +1,10 @@
+"use server";
+
+import { getFormObject } from "@/lib/utils";
+
 export async function login(state: unknown, formData: FormData) {
   console.log("Login");
+  const obj = getFormObject(formData);
   await new Promise((res) => setTimeout(res, 1000));
+  console.log(obj);
 }
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+type FormRecord = {
+  [key: string]: FormDataEntryValue | FormDataEntryValue[] | undefined;
+};
+
+/** Helper function to transform FormData into a JavaScript object. */
+export function getFormObject(form: FormData): FormRecord {
+  const result: FormRecord = {};
+  for (const [key, value] of form.entries()) {
+    const cur = result[key];
+    if (cur === undefined) {
+      // Not present in object
+      result[key] = value;
+    } else if (cur instanceof Array) {
+      // Multiple values already present
+      cur.push(value);
+    } else {
+      // First occurence o multiple values
+      result[key] = [cur, value];
+    }
+  }
+
+  return result;
 }
