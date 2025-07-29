e905d5e6509639ef33b6fb512a00d1f288013cdb
Vincent <vafdaf11@gmail.com>
2025-07-29 23:49:07 +0200

    feat: Fastest index in the west

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,72 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div>
+      <Card className="max-w-md mx-auto mt-6">
+        <CardHeader>
+          <CardTitle>Welcome!</CardTitle>
+        </CardHeader>
+        <CardContent className="gap-2 flex flex-col">
           <Button asChild>
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Deploy now
-            </a>
+            <a href="/signup">Sign Up</a>
           </Button>
-          <Button asChild variant="secondary">
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read our docs
-            </a>
+          <Button asChild>
+            <a href="/login">Login</a>
           </Button>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <Button asChild variant="link">
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn
-          </a>
-        </Button>
-        <Button asChild variant="link">
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Examples
-          </a>
-        </Button>
-        <Button asChild variant="link">
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Go to nextjs.org →
-          </a>
-        </Button>
-      </footer>
+          <Button asChild>
+            <a href="/alerts">Alerts</a>
+          </Button>
+          <Button asChild>
+            <a href="/profile">Profile</a>
+          </Button>
+          <Button asChild>
+            <a href="/travelRisk">Travel Risk</a>
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
