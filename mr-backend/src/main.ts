3bdd20ea9227cb933581ef7c4fa668a445513c6d
Michael Horwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2025-07-29 10:46:51 +0200

    init user controller and service (#10)
    * init
    
    * move to yarn
    
    * db init
    
    * init user controller and service

---
+++ mr-backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import 'reflect-metadata'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Travel Risk Api')
+    .setDescription('See title')
+    .setVersion('1.0')
+    .addTag('dev')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
---
+++ mr-backend/src/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+import { Repository } from "typeorm";
+import { CreateUserDto, UpdateUserDto } from "./dto/create-user.dto";
+
+// src/user/user.service.ts
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repo: Repository<User>,
+  ) {}
+
+  findAll()            { return this.repo.find(); }
+  findOne(id: number)  { return this.repo.findOneBy({ id }); }
+  create(dto: CreateUserDto) { return this.repo.save(dto); }
+  update(id: number, dto: UpdateUserDto) {
+    return this.repo.save({ ...dto, id });
+  }
+  async remove(id: number) {
+    await this.repo.delete(id);
+  }
+}
